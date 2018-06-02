73013af7f71ac1b547aade543d7f7e972523b96f
sicmy <sicmy.kwon@gmail.com>
2018-06-02 07:47:20 +0000

    change container

--- node/server.js
+++ node/server.js
@@ -1,13 +1,19 @@
 'use strict';
+
 const express = require('express');
+
+const os = require('os');
+var hostname = os.hostname();
 
 const PORT = 8000;
 const HOST = '0.0.0.0';
 
 const app = express();
 app.get('/', (req, res) => {
-  res.send('펠릭스쏭은 대단해!!!\n');
+  res.send("<html><head><h1>hostname : " + hostname +
+    	"</h1></head><body><img src='https://blog.docker.com/wp-content/uploads/Swarmnado-357x627-30-1.gif'></body></html>");
 });
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
+
